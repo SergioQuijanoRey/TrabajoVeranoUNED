466a250355c0ffadc233cd0d07b19f1bd99f6e19
Sergio Quijano Rey <sergiquijano@gmail.com>
2020-07-23 17:22:54 +0200

    docs: documento la clase Localizacion

--- src/Etapa 1/Localizacion.java
+++ src/Etapa 1/Localizacion.java
@@ -1,4 +1,7 @@
 
+/**
+ * Clase que representa una localizacion geografica
+ * */
 public class Localizacion{
     // Atributos
     //==========================================================================
@@ -9,6 +12,9 @@ public class Localizacion{
 
     // Constructores
     //==========================================================================
+    /**
+     * Constructor de la clase
+     * */
     Localizacion(String Nombre, double Latitud, double Longitud, double Altitud){
         this.Nombre = Nombre;
         this.Latitud = Latitud;
@@ -18,42 +24,70 @@ public class Localizacion{
 
     // Getters
     //==========================================================================
+    /**
+     * Getter del nombre de la localizacion
+     * */
     public String getNombre(){
         return this.Nombre;
     }
 
+    /**
+     * Getter de la latitud de la localizacion
+     * */
     public double getLatitud(){
         return this.Latitud;
     }
 
+    /**
+     * Getter de la longitud de la localizacion
+     * */
     public double getLongitud(){
         return this.Longitud;
     }
 
+    /**
+     * Getter de la altitud de la localizacion
+     * */
     public double getAltitud(){
         return this.Altitud;
     }
 
     // Setters
     //==========================================================================
+    /**
+     * Modifica el nombre de la localizacion
+     * */
     public void setNombre(String Nombre){
         this.Nombre = Nombre;
     }
 
+    /**
+     * Modifica la latitud de la localizacion
+     * */
     public void setLatitud(double Latitud){
         this.Latitud = Latitud;
     }
 
+    /**
+     * Modifica la longitud de la localizacion
+     * */
     public void setLongitud(double Longitud){
         this.Longitud = Longitud;
     }
 
+    /**
+     * Modifica la altitud de la localizacion
+     * */
     public void setAltitud(double Altitud){
         this.Altitud = Altitud;
     }
 
     // Otros metodos
     //==========================================================================
+    /**
+     * Muestra informacion de la localizacion
+     * @return un String multilinea con la informacion de la localizacion
+     * */
     public String print(){
         String data = "";
         data = data + "Longitud: " + String.valueOf(this.Longitud) + "\n";
