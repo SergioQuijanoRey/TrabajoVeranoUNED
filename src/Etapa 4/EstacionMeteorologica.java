6b2aad920bef94fd8a2bc40aa3d7c5a706eb11d3
Sergio Quijano Rey <sergiquijano@gmail.com>
2020-08-08 18:33:03 +0200

    refactor: termino de corregir la clae EstacionMeteorologica al nuevo polimorfismo

--- src/Etapa 4/EstacionMeteorologica.java
+++ src/Etapa 4/EstacionMeteorologica.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 /**
  * Clase que representa una estacion meteorologica
  *
- * Una estacion meteorologica se compone de los tres instrumentos y tres listas
- * de mediciones que se gestionan totalmente independientes de las listas de mediciones
- * que los instrumentos de la estacion gestionan
+ * Una estacion meteorologica se compone de sensores y una lista de mediciones que
+ * se gestionan totalmente independientes de las listas de mediciones que los
+ * instrumentos de la estacion gestionan
  *
  * Por ello es posible usar mal esta clase y añadir valores a instrumentos y no
  * añadirlos correctamente a la estacion meteorologica
@@ -50,45 +50,17 @@ public class EstacionMeteorologica{
     }
 
     /**
-     * Getter del termometro de la estacion
+     * Getter de los sensores de la estacion
      * */
-    public Termometro getTermometro(){
-        return this.termometro;
+    public ArrayList<Sensores> getSensores(){
+        return this.sensores;
     }
 
     /**
-     * Getter del pluviometro de la estacion
+     * Getter de las mediciones registradas en la estacion
      * */
-    public Pluviometro getPluviometro(){
-        return this.pluviometro;
-    }
-
-    /**
-     * Getter del detector de N2O de la estacion
-     * */
-    public DetectorN2O getDetectorN2O(){
-        return this.detector;
-    }
-
-    /**
-     * Getter de los valores medidos de temperatura
-     * */
-    public ArrayList<Medicion> getValoresTemperatura(){
-        return this.valoresTemperatura;
-    }
-
-    /**
-     * Getter de los valores medidos de temperatura
-     * */
-    public ArrayList<Medicion> getValoresPrecipitaciones(){
-        return this.valoresPrecipitaciones;
-    }
-
-    /**
-     * Getter de los valores medidos de concentracion de N2O
-     * */
-    public ArrayList<Medicion> getValoresN2O(){
-        return this.valoresN2O;
+    public ArrayList<Medicion> getValores(){
+        return this.valores;
     }
 
     // Setters
@@ -108,103 +80,36 @@ public class EstacionMeteorologica{
     }
 
     /**
-     * Modifica el termometro de la estacion
+     * Modifica manualmente el array de sensores de la estacion
      * */
-    public void setTermometro(Termometro termometro){
-        this.termometro = termometro;
+    public void setSensores(ArrayList<Sensor> sensores){
+        this.sensores = sensores;
     }
 
     /**
-     * Modifica el pluviometro de la estacion
+     * Modifica manualemnte el array de mediciones de la estacion
+     * Se desaconseja el uso de este metodo
+     * Es mejor idea usar el metodo addMedicion para modificar la lista de mediciones
      * */
-    public void setPluviometro(Pluviometro pluviometro){
-        this.pluviometro = pluviometro;
+    public void setValores(ArrayList<Medicion> valores){
+        this.valores = valores;
     }
 
-    /**
-     * Modifica el detector de gases de la estacion
-     * */
-    public void setDetectorN2O(DetectorN2O detector){
-        this.detector = detector;
-    }
-
-    /**
-     * Modifica los valores de temperatura de la estacion
-     * */
-    public void setValoresTemperatura(ArrayList<Medicion> valoresTemperatura){
-        this.valoresTemperatura = valoresTemperatura;
-    }
-
-    /**
-     * Modifica los valores de precipitaciones de la estacion
-     * */
-    public void setValoresPrecipitaciones(ArrayList<Medicion> valoresPrecipitaciones){
-        this.valoresPrecipitaciones = valoresPrecipitaciones;
-    }
-
-    /**
-     * Modifica los valores de concentracion de N2O de la estacion
-     * */
-    public void setValoresN2O(ArrayList<Medicion> valoresN2O){
-        this.valoresN2O = valoresN2O;
-    }
 
     // Otros metodos
     //==========================================================================
-    /**
-     * Halla el valor mas alto registrado de temperatura, y devuelve su valor medido
-     * Si no hay valores medidos, se devuelve por defecto un cero
-     *
-     * @return el valor mas alto medido de temperatura
-     * */
-    public double valorMasAltoT(){
-        // Comprobacion de seguridad
-        if(this.valoresTemperatura == null || this.valoresTemperatura.size() == 0){
-            return 0.0;
-        }
-
-        // Iteramos buscando el maximo
-        double max = this.valoresTemperatura.get(0).getValorMedido();
-        for(int i = 0; i < this.valoresTemperatura.size(); i++){
-            if(this.valoresTemperatura.get(i).getValorMedido() > max){
-                max = valoresTemperatura.get(i).getValorMedido();
-            }
-        }
-
-        return max;
-    }
 
     /**
-     * Halla el valor mas alto registrado de precipitaciones, y devuelve su valor medido
+     * Halla el valor mas alto registrado de una unidad dada, y devuelve su valor
+     * medido
      * Si no hay valores medidos, se devuelve por defecto un cero
      *
-     * @return el valor mas alto medido de precipitaciones
-     * */
-    public double valorMasAltoP(){
-        // Comprobacion de seguridad
-        if(this.valoresPrecipitaciones == null || this.valoresPrecipitaciones.size() == 0){
-            return 0.0;
-        }
-
-        // Iteramos buscando el maximo
-        double max = this.valoresPrecipitaciones.get(0).getValorMedido();
-        for(int i = 0; i < this.valoresPrecipitaciones.size(); i++){
-            if(this.valoresPrecipitaciones.get(i).getValorMedido() > max){
-                max = valoresPrecipitaciones.get(i).getValorMedido();
-            }
-        }
-
-        return max;
-
-    }
-
-    /**
-     * Halla el valor mas alto registrado de concentracion de gas, y devuelve su valor medido
-     * Si no hay valores medidos, se devuelve por defecto un cero
+     * @param unidadMedida la unidad de medida que se quiere comprobar
+     * @return el valor mas alto medido de dicha medida
      *
-     * @return el valor mas alto medido de concentracion de gas
+     * TODO -- Falta por implementar
      * */
-    public double valorMasAltoN2O(){
+    public double valorMasAlto(UnidadMedida unidadMedida){
         // Comprobacion de seguridad
         if(this.valoresN2O == null || this.valoresN2O.size() == 0){
             return 0.0;
